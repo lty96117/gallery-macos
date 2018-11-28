355ad028e7754356b2acf431550fb41813396e09
lty96117 <18642966+lty96117@users.noreply.github.com>
2018-11-28 14:55:26 +0800

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Tianyu Li on 11/28/18.
+//  Copyright © 2018 Tianyu Li. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
