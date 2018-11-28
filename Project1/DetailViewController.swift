c59c45a6cdd15b74dc4044dfb50ea7196373b16c
lty96117 <18642966+lty96117@users.noreply.github.com>
2018-11-28 17:00:09 +0800

    UI

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Tianyu Li on 11/28/18.
+//  Copyright © 2018 Tianyu Li. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    @IBOutlet var imageView: NSImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Project1/SourceViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SourceViewController.swift
+//  Project1
+//
+//  Created by Tianyu Li on 11/28/18.
+//  Copyright © 2018 Tianyu Li. All rights reserved.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    @IBOutlet var tableView: NSTableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    
+    
+}
